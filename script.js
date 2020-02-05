9867df85247c3c4631bac237276847ed87c6529d
YuTingDong <luke.dong@mail.searching-service.com>
2020-02-05 11:21:21 +0800

    update sweetalert

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var play = document.querySelector("#play");
 
 play.onclick = function() {
     if(cat1) {
-        swal("現在...", "可以下載喜歡的貓貓圖囉", "success");
+        swal("可以下載喜歡的貓貓圖囉", "滑鼠移至貓貓圖=>右鍵=>另存圖檔", "success");
         play.src = "https://image.flaticon.com/icons/svg/2468/2468825.svg"
         clearInterval(cat1);
         cat1 = null;
